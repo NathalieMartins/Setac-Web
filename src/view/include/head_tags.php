735394db97e4b064b99da92f73e1f2962ddec42f
Mikael Messias <mikaelmessias@outlook.com>
2019-07-10 20:41:38 -0300

    Adiciona diretório src/view/include
    Esse diretório armazena os arquivos a serem incluídos por padrão nas
    páginas:
    
    	1. title_config: configura o título das páginas.
    	2. head_tags: adiciona as tags da seção head.
    	3. navigation: adiciona a barra de navegação do sistema.
    
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

---
+++ src/view/include/head_tags.php
@@ -0,0 +1,11 @@
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Os títulos das páginas são definidos em view/include/title_config.php -->
+<title>
+  <?php echo $TITULO_PAGINA; ?>
+</title>
+
+<link rel="stylesheet" type="text/css" href="/Setac-Web/node_modules/bulma/css/bulma.min.css">
+  
+<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
